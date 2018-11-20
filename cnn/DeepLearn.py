8f54e07317e0feb4a0eb266d782656e5f3a54df0
[ShenTaoSd] <[2115219121@qq.com]>
2018-11-20 08:27:39 +0800

    hello

--- cnn/DeepLearn.py
+++ cnn/DeepLearn.py
@@ -11,14 +11,5 @@ plt.rcParams['text.hinting_factor'] = 8
 # 产生分布
 fit = plt.figure()
 
-figa = plt.gca()
 
-N = 100
-xn = np.random.rand(100)
-x = np.linspace(0, 1)
 
-a = np.random.rand()
-b = np.random.rand()
-
-f = x * a + b;
-plt.plot(x, f, 'r')
